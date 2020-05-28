c38f38a9f33b5f53b8dd3769bc99ba888cdeae82
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-05-28 12:06:13 +0530

    Added : Mutlipal Times Heads And Tails Using While Loop

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,28 @@
 #!/bin/bash -x
 
+
 Heads=0
 Tails=1
 
-fliping_coin=$((RANDOM%2))
 
-if [ $fliping_coin -eq $Heads ]
-then
-    echo "Heads"
-else
-    echo "Tails"
-fi
+
+count=0
+A=0
+B=0
+c=10
+
+while [ $count ]
+   fliping_coin=$((RANDOM%2))
+do
+    if [ $fliping_coin -eq $Heads ]
+     then
+         echo " Heads = $A "
+            ((A++))
+    elif [ $fliping_coin -eq $Tails ]
+     then
+         echo " Tails = $B "
+            ((B++))
+    fi
+       ((count++))
+done
+
